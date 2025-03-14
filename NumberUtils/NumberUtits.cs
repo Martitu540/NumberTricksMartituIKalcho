31115c793a95f9fb68fa792f33cda85b920d8937
MEGA <MEGA@gmail.com>
2025-03-14 11:51:49 +0200

    bring back code from main

--- NumberUtils/NumberUtits.cs
+++ NumberUtils/NumberUtits.cs
@@ -3,13 +3,13 @@
 namespace NumberTricks
 {
 
-public class NumberTricks
+    public class NumberTricks
     {
-		
-	public NumberTricks()
-	{
-		
-    }
+
+        public NumberTricks()
+        {
+
+        }
         public static double FastPow(double a, int n)
         {
             if (n == 0)
@@ -21,6 +21,20 @@ public class NumberTricks
             return n % 2 == 0 ? subTotal : subTotal * a;
         }
 
-    } 
+        public double NRoothBabylon(int n, int num)
+        {
+            double ans = num;
+            double end = 0;
+            double doKolko = 0.000001;
+            double mnogo = double.MaxValue; ;
+            while (mnogo > doKolko)
+            {
+                double prev = ans;
+                ans = ((n - 1) * ans + num / Math.Pow(ans, n - 1)) / n;
+                mnogo = Math.Abs(ans - prev);
+
+            }
+			return ans;}
+        }
 
 }

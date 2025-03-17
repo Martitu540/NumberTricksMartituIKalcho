385d87c944cdade9e717fa3723eb549359583695
MEGA <MEGA@gmail.com>
2025-03-17 12:12:37 +0200

    koda e gotov

--- NumberUtils/NumberUtits.cs
+++ NumberUtils/NumberUtits.cs
@@ -3,30 +3,54 @@
 namespace NumberTricks
 {
 
-public class NumberUtilts
-{
-		private int n, num;
-	public NumberUtilts(int n, int num)
-	{
-		this.n = n;
+    public class NumberUtilts
+    {
+        private int n, num;
+        public NumberUtilts(int n, int num)
+        {
+            this.n = n;
             this.num = num;
         }
 
-		public double NRoothBabylon(int n, int num)
-		{
-			double ans = num;
-			double end = 0;
-			double doKolko = 0.000001;
-			double mnogo = double.MaxValue; ;
-            while(mnogo> doKolko)
-			{
-				double prev = ans;
-				ans = ((n-1) * ans+ num/Math.Pow(ans, n-1))/n;
+        public double NRoothBabylon(int n, int num)
+        {
+            double ans = num;
+            double end = 0;
+            double doKolko = 0.000001;
+            double mnogo = double.MaxValue; ;
+            while (mnogo > doKolko)
+            {
+                double prev = ans;
+                ans = ((n - 1) * ans + num / Math.Pow(ans, n - 1)) / n;
                 mnogo = Math.Abs(ans - prev);
             }
 
-			return ans;
+            return ans;
         }
-} 
-
+        public static void SieveOfEratosthenes(int m)
+        {
+            bool[] prime = new bool[m + 1];
+            for (int i=0; i <= m; i++)
+            {
+                prime[i] = true;
+            }
+            for (int p = 2; p * p <= m; p++)
+            {
+                if (prime[p] == true)
+                {
+                    for (int i = p * p; i <= m; i += p)
+                    {
+                        prime[i] = false;
+                    }
+                }
+            }
+            for (int i = 2; i <= m; i++)
+            {
+                if (prime[i] == true)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+    }
 }
--- NumberUtils/Program.cs
+++ NumberUtils/Program.cs
@@ -7,7 +7,12 @@
             int n = int.Parse(Console.ReadLine());
             int num = int.Parse(Console.ReadLine());
             NumberUtilts numberUtilts = new NumberUtilts(n, num);
-            Console.WriteLine(numberUtilts.NRoothBabylon(n,num));
+            Console.WriteLine(numberUtilts.NRoothBabylon(n, num));
+
+
+            Console.WriteLine("SieveOfEratosthenes: ");
+            int m = int.Parse(Console.ReadLine());
+            NumberUtilts.SieveOfEratosthenes(m);
         }
     }
 }
